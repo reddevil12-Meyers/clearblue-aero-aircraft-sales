39c8d2e2e5f8f1b54bd80bef0fe0a0156598967f
John Secord <secord.john@gmail.com>
2026-08-09 21:52:48 +0000

    File changes

--- src/pages/public/Home.jsx
+++ src/pages/public/Home.jsx
@@ -67,8 +67,8 @@ export default function PublicHome() {
         
         <div className="relative z-10 max-w-5xl mx-auto">
           <p className="text-[hsl(var(--background))] mb-10 mx-auto leading-tight opacity-100 max-w-2xl">
-            <span className="block font-black mb-3 pt-10 text-[2.8rem]"> Aircraft Sales & Ownership Through Our Specialized, Trusted Services</span>
-            <span className="block mt-3 text-2xl">Give us a try and see why we are quickly becoming your aircraft brokerage firm of choice!</span>
+            <span className="block font-black mb-3 pt-10 text-2xl sm:text-3xl md:text-4xl lg:text-5xl"> Aircraft Sales & Ownership Through Our Specialized, Trusted Services</span>
+            <span className="block mt-3 text-base sm:text-lg md:text-xl">Give us a try and see why we are quickly becoming your aircraft brokerage firm of choice!</span>
           </p>
           <div className="flex flex-wrap justify-center gap-4 pb-12">
             <Link to="/inventory"
